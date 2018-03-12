37f049862b647da8dc1276fd3507e1eca499645a
kersulec <yoann.kersulec@epitech.eu>
2018-03-12 12:01:01 +0100

    Int -> unsigned char in AEntity.hpp

--- AEntity.hpp
+++ AEntity.hpp
@@ -15,11 +15,11 @@ namespace arcade {
 	public:
 		virtual arcade::Point <float> getPos() const = 0;
 		virtual float getAngle() const = 0;
-		virtual int getKeyTraductor() const = 0;
+		virtual unsigned char getKeyTraductor() const = 0;
 	protected:
 		arcade::Point <float>pos;
 		float angle;
-		int key_traductor;
+		unsigned char key_traductor;
 	};
 }
 
