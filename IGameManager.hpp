e62b06edbb8a9744226dd15447631875f47bd89b
Bernard erwan <erwan.bernard@epitech.net>
2018-04-04 15:08:39 +0200

    add return and arg in GUI and add constness in setplayername

--- IGameManager.hpp
+++ IGameManager.hpp
@@ -33,8 +33,8 @@ namespace arcade {
 		virtual const std::vector<arcade::AEntity *> &getEntities() const = 0;
 		virtual const std::vector<std::vector<unsigned char>> &getMap() const = 0;
 		virtual const std::unordered_map<unsigned char, Traductor> &getTraductor() const = 0;
-		virtual void setPlayerName(std::string &name) = 0;
-		virtual void getUI() const = 0;
+		virtual void setPlayerName(const std::string &name) = 0;
+		virtual const guiInfo &getGUI() const = 0;
 	};
 }
 /*
